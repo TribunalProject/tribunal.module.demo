2633e11bedd9af58bbbe3e4bd93bcf021fe13d7f
c-ardinal <0rdinal.inf0tec.e1ectr0nics@gmail.com>
2017-11-05 01:21:18 +0900

    Add source code and build files

---
+++ src/main/java/tribunal/plugin/demo/Demo2.java
@@ -0,0 +1,19 @@
+package tribunal.plugin.demo;
+
+import tribunal.core.Verdict;
+
+public class Demo2 implements Verdict {
+
+    public void init(){
+        System.out.println(this.getClass().getName() + " - Demo2 verdict init.");
+    }
+
+    public Object call(Object[] args){
+        System.out.println(this.getClass().getName() + " - Demo2 verdict call.");
+        for(Object arg: args) {
+            System.out.println(this.getClass().getName() + " - " + arg);
+        }
+        return null;
+    }
+
+}
